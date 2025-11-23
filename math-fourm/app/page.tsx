8b9a0bdb5405602929903653f2fb28179d970217
Polygonal <154336865+Roman-Stanuch@users.noreply.github.com>
2025-11-22 23:23:08 -0600

    Finished explore page

--- math-fourm/app/page.tsx
+++ math-fourm/app/page.tsx
@@ -6,11 +6,13 @@ import db from "@/lib/db"
 export default function Home() {
   var states = [{}]
   var authors = []
+  var titles = []
   for (let i = 0; i < 16; i++) {
     const row = db.prepare("SELECT * FROM posts ORDER BY RANDOM() LIMIT 1").get()
     if (row && row.content) {
       states[i] = row.content
       authors[i] = row.author
+      titles[i] = row.title
     } else {
       i--
     }
@@ -26,22 +28,22 @@ export default function Home() {
             strategy="afterInteractive"
           />
           <div className="grid grid-cols-4 gap-6 bg-gray-200">
-            <Project state={states[0]} user={authors[0]} projectName="Hello World"></Project>
-            <Project state={states[1]} user={authors[1]} projectName="Hello World"></Project>
-            <Project state={states[2]} user={authors[2]} projectName="Hello World"></Project>
-            <Project state={states[3]} user={authors[3]} projectName="Hello World"></Project>
-            <Project state={states[4]} user={authors[4]} projectName="Hello World"></Project>
-            <Project state={states[5]} user={authors[5]} projectName="Hello World"></Project>
-            <Project state={states[6]} user={authors[6]} projectName="Hello World"></Project>
-            <Project state={states[7]} user={authors[7]} projectName="Hello World"></Project>
-            <Project state={states[8]} user={authors[8]} projectName="Hello World"></Project>
-            <Project state={states[9]} user={authors[9]} projectName="Hello World"></Project>
-            <Project state={states[10]} user={authors[10]} projectName="Hello World"></Project>
-            <Project state={states[11]} user={authors[11]} projectName="Hello World"></Project>
-            <Project state={states[12]} user={authors[12]} projectName="Hello World"></Project>
-            <Project state={states[13]} user={authors[13]} projectName="Hello World"></Project>
-            <Project state={states[14]} user={authors[14]} projectName="Hello World"></Project>
-            <Project state={states[15]} user={authors[15]} projectName="Hello World"></Project>
+            <Project state={states[0]} user={authors[0]} projectName={titles[0]}></Project>
+            <Project state={states[1]} user={authors[1]} projectName={titles[1]}></Project>
+            <Project state={states[2]} user={authors[2]} projectName={titles[2]}></Project>
+            <Project state={states[3]} user={authors[3]} projectName={titles[3]}></Project>
+            <Project state={states[4]} user={authors[4]} projectName={titles[4]}></Project>
+            <Project state={states[5]} user={authors[5]} projectName={titles[5]}></Project>
+            <Project state={states[6]} user={authors[6]} projectName={titles[6]}></Project>
+            <Project state={states[7]} user={authors[7]} projectName={titles[7]}></Project>
+            <Project state={states[8]} user={authors[8]} projectName={titles[8]}></Project>
+            <Project state={states[9]} user={authors[9]} projectName={titles[9]}></Project>
+            <Project state={states[10]} user={authors[10]} projectName={titles[10]}></Project>
+            <Project state={states[11]} user={authors[11]} projectName={titles[11]}></Project>
+            <Project state={states[12]} user={authors[12]} projectName={titles[12]}></Project>
+            <Project state={states[13]} user={authors[13]} projectName={titles[13]}></Project>
+            <Project state={states[14]} user={authors[14]} projectName={titles[14]}></Project>
+            <Project state={states[15]} user={authors[15]} projectName={titles[15]}></Project>
           </div>
         </div>
       </div>
