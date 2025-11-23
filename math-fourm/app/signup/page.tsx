d0335eaed5923480aeb4f0bf322fb718cb73d125
Henry Veedahl <89225651+HenryNerd@users.noreply.github.com>
2025-11-23 02:39:50 -0600

    Unify Changes
    Co-Authored-By: Michael <53538532+Mikeymoo5@users.noreply.github.com>

--- math-fourm/app/signup/page.tsx
+++ math-fourm/app/signup/page.tsx
@@ -1,22 +1,41 @@
 'use server'
 import { create_account } from '@/app/actions/create_account'
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Button } from '@/components/ui/button'
+import Navbar from '@/components/navbar'
 
 export default async function SignupPage() {
     return (
-        <form action={create_account}>
-            <div>
-                <label htmlFor='realname'>Name</label>
-                <input id="realname" name="realname"/>
+        <div className='bg-gray-200'>
+            <Navbar username="No Acount"></Navbar>
+            <div className='flex justify-center items-center'>
+                <Card className='w-[500px] bg-gray-100'>
+                    <form action={create_account}>
+                        <div>
+                            <label htmlFor='realname'>Name</label>
+                            <Input className="w=[475px]" id="realname" name="realname" />
+                        </div>
+                        <div>
+                            <label htmlFor='username'>Username</label>
+                            <Input id="username" name="username" />
+                        </div>
+                        <div>
+                            <label htmlFor="password">Password</label>
+                            <Input type="password" id="password" name="password" />
+                        </div>
+                        <Button type="submit">Sign up</Button>
+                    </form>
+                </Card>
             </div>
-            <div>
-                <label htmlFor='username'>Username</label>
-                <input id="username" name="username"/>
-            </div>
-            <div>
-                <label htmlFor="password">Password</label>
-                <input id="password" name="password"/>
-            </div>
-            <button type="submit">Sign up!</button>
-        </form>
+        </div>
     )
 }
