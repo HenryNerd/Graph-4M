291718f07d08935d35ecb211fb85ad6dfd9b2718
Henry Veedahl <89225651+HenryNerd@users.noreply.github.com>
2025-11-23 08:02:09 -0600

    Add Base Post Page Card

--- math-fourm/app/posts/[post_id]/page.tsx
+++ math-fourm/app/posts/[post_id]/page.tsx
@@ -3,6 +3,15 @@ import Viewer from "@/components/viewer";
 import Navbar from "@/components/navbar";
 import { auth } from "@/auth"
 import { redirect } from "next/navigation";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 export default async function Post({
     params,
@@ -12,18 +21,33 @@ export default async function Post({
     const session = await auth();
     console.log(session)
     const username = session?.user?.name || "Guest"
-    
     const { post_id } = await params
     const row = db.prepare("SELECT * FROM posts WHERE title = ?").get(post_id)
-    
+
     if (row == undefined) {
         redirect("/");
     }
-    
+
     return (
         <div>
-            <Navbar username={username}></Navbar>
-            <Viewer state={row.content}></Viewer>
+            <Navbar></Navbar>
+            <div className="justify-center">
+            <div className="w-3/4 h-[100px] border-l ml-3 mb-4">
+                <Card className="h-[100px]">
+                    <CardHeader>
+                        <CardTitle className="text-2xl">Project Name</CardTitle>
+                        <CardDescription className="text-md">By: @user</CardDescription>
+                    </CardHeader>
+                </Card>
+            </div>
+            <div className="w-4/4 h-screen">
+                <div className="w-full h-[600px] flex flex-col">
+                    <div className="flex-1 overflow-auto">
+                        <Viewer state={row.content}></Viewer>
+                    </div>
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
--- math-fourm/components/viewer.tsx
+++ math-fourm/components/viewer.tsx
@@ -2,6 +2,15 @@
 
 import Script from "next/script";
 import React, { useRef } from "react";
+import {
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
 
 declare global {
     interface Window {
@@ -9,11 +18,11 @@ declare global {
     }
 }
 
-export default function Viewer({state}: any) {
+export default function Viewer({ state }: any) {
     const calcRef = useRef(null);
     const onScriptLoad = () => {
         if (window.Desmos && calcRef.current) {
-            const calculator = new window.Desmos.GraphingCalculator(calcRef.current, {actions: false});
+            const calculator = new window.Desmos.GraphingCalculator(calcRef.current, { actions: false });
             calculator.setState(state);
         }
     };
@@ -21,8 +30,11 @@ export default function Viewer({state}: any) {
     return (
         <div className="overflow-hidden">
             <div className="align-bottom">
-                <Script src="https://www.desmos.com/api/v1.11/calculator.js?apiKey=c1a0cb85f3d54439ac59648737fd0bb3" strategy="afterInteractive" onLoad={onScriptLoad} />
-                <div id="calculator" ref={calcRef} className="w-3/4 h-[700px]"></div>
+
+                <div>
+                    <Script src="https://www.desmos.com/api/v1.11/calculator.js?apiKey=c1a0cb85f3d54439ac59648737fd0bb3" strategy="afterInteractive" onLoad={onScriptLoad} />
+                    <div id="calculator" ref={calcRef} className="w-3/4 h-[700px] ml-3"></div>
+                </div>
             </div>
         </div>
     );
