d8285b0278a14f10c361deb8f54185319de98126
Henry Veedahl <89225651+HenryNerd@users.noreply.github.com>
2025-11-23 03:06:31 -0600

    Added UI Components

--- math-fourm/app/login/page.tsx
+++ math-fourm/app/login/page.tsx
@@ -2,27 +2,40 @@ import { signIn } from "@/auth"
 import { auth } from "@/auth"
 import { RedirectStatusCode } from "next/dist/client/components/redirect-status-code"
 import { redirect } from "next/navigation"
+import { AlertCircleIcon } from "lucide-react"
+import {
+    Alert,
+    AlertDescription,
+    AlertTitle,
+} from "@/components/ui/alert"
 
 export default async function SignIn() {
     // const session = await auth()
     // console.log(session)
     return (
         <form
-        action={async (formData) => {
-            "use server"
+            action={async (formData) => {
+                "use server"
                 await signIn("credentials", formData)
-        }} 
+            }}
         >
-        <input type="hidden" name="redirectTo" value="/" />
-        <label>
-            Email
-            <input name="username" type="text" />
-        </label>
-        <label>
-            Password
-            <input name="password" type="password" />
-        </label>
-        <button>Sign In</button>
+            <Alert variant="destructive">
+                <AlertCircleIcon />
+                <AlertTitle>Acount Not Found</AlertTitle>
+                <AlertDescription>
+                    Please check your username and password.
+                </AlertDescription>
+            </Alert>
+            <input type="hidden" name="redirectTo" value="/" />
+            <label>
+                Email
+                <input name="username" type="text" />
+            </label>
+            <label>
+                Password
+                <input name="password" type="password" />
+            </label>
+            <button>Sign In</button>
         </form>
     )
 }
--- math-fourm/app/posts/[post_id]/page.tsx
+++ math-fourm/app/posts/[post_id]/page.tsx
@@ -1,43 +1,28 @@
 import db from "@/lib/db"
 import Viewer from "@/components/viewer";
 import Navbar from "@/components/navbar";
-
+import { auth } from "@/auth"
 import { redirect } from "next/navigation";
-
-const state = {
-    version: 10,
-    randomSeed: "3ff2f425b09c2bb2b",
-    graph: {
-        viewport: {
-            xmin: -10,
-            ymin: -10,
-            xmax: 10,
-            ymax: 10
-        }
-    },
-    expressions: {
-        list: [
-            { id: "1", type: "expression", latex: "y=x^2" },
-            { id: "2", type: "expression", latex: "y=\\sin(x)" }
-        ]
-    }
-};
 
 export default async function Post({
     params,
-}: { 
-    params: Promise<{post_id: string}>
+}: {
+    params: Promise<{ post_id: string }>
 }) {
+    const session = await auth();
+    console.log(session)
+    const username = session?.user?.name
+    
     const { post_id } = await params
     const row = db.prepare("SELECT * FROM posts WHERE title = ?").get(post_id)
-
+    
     if (row == undefined) {
         redirect("/");
     }
-
+    
     return (
         <div>
-            <Navbar></Navbar>
+            <Navbar username={username}></Navbar>
             <Viewer state={row.content}></Viewer>
         </div>
     )
