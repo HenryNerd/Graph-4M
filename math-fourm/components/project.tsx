ce15bdf1b151335381479e4c6f7f8f8ddb96e402
Henry Veedahl <89225651+HenryNerd@users.noreply.github.com>
2025-11-22 18:20:06 -0600

    Combine projectDesmos and project componenets

--- math-fourm/components/project.tsx
+++ math-fourm/components/project.tsx
@@ -1,23 +1,56 @@
+"use client";
+
 import {
-  Card,
-  CardAction,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
+    Card,
+    CardAction,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
 } from "@/components/ui/card"
-import Desmos from "./projectDesmos";
+import Script from "next/script";
+import { useRef } from "react";
+
+declare global {
+    interface Window {
+        Desmos: any;
+    }
+}
 
 type ProjectProps = {
     state: any;
 };
 
 export default function Project({ state }: ProjectProps) {
+    const calcRef = useRef<HTMLDivElement>(null);
+    const calculatorRef = useRef<any>(null);
+
+    const onScriptLoad = () => {
+        if (window.Desmos && calcRef.current) {
+            calculatorRef.current = new window.Desmos.GraphingCalculator(calcRef.current, {
+                expressions: false,
+                zoomButtons: false,
+                settingsMenu: false,
+                lockViewport: true,
+            });
+
+            calculatorRef.current.setState(state);
+        }
+    };
+
+
     return (
         <Card className="w-[300px] h-[350px]">
             <CardContent>
-                <Desmos state={state} />
+                <div className="align-bottom">
+                    <Script
+                        src="https://www.desmos.com/api/v1.11/calculator.js?apiKey=c1a0cb85f3d54439ac59648737fd0bb3"
+                        strategy="afterInteractive"
+                        onLoad={onScriptLoad}
+                    />
+                    <div id="calculator" ref={calcRef} className="w-[250px] h-[250px]"></div>
+                </div>
                 <CardTitle className="mt-4">Project Name</CardTitle>
                 <CardDescription>By: User</CardDescription>
             </CardContent>
