1bcf6080b647539968324995ad7f7d7275f57a50
Henry Veedahl <89225651+HenryNerd@users.noreply.github.com>
2025-11-23 05:08:12 -0600

    Start Post Page Fixes

--- math-fourm/components/viewer.tsx
+++ math-fourm/components/viewer.tsx
@@ -13,7 +13,7 @@ export default function Viewer({state}: any) {
     const calcRef = useRef(null);
     const onScriptLoad = () => {
         if (window.Desmos && calcRef.current) {
-            const calculator = new window.Desmos.GraphingCalculator(calcRef.current, {expressions: true});
+            const calculator = new window.Desmos.GraphingCalculator(calcRef.current, {actions: false});
             calculator.setState(state);
         }
     };
@@ -22,7 +22,7 @@ export default function Viewer({state}: any) {
         <div className="overflow-hidden">
             <div className="align-bottom">
                 <Script src="https://www.desmos.com/api/v1.11/calculator.js?apiKey=c1a0cb85f3d54439ac59648737fd0bb3" strategy="afterInteractive" onLoad={onScriptLoad} />
-                <div id="calculator" ref={calcRef} className="w-screen h-screen"></div>
+                <div id="calculator" ref={calcRef} className="w-3/4 h-[700px]"></div>
             </div>
         </div>
     );
